0167ed0ee3ad341397105517475315807c24657f
kanakoHub <69960209+kanakoHub@users.noreply.github.com>
2020-10-31 01:23:53 +0900

    Add files via upload

--- assets/contact/contact.php
+++ assets/contact/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <zhensongjianaizi@gmail.com>'; 
-$sendTo = 'Demo contact form <zhensongjianaizi@gmail.com>';
+$from = 'Demo contact form <demo@domain.com>'; 
+$sendTo = 'Demo contact form <demo@domain.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
